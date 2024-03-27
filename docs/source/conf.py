f9bb4ddae20c5bf7dd322291c171a83c7287e01b
Xavier Mouy <xavier.mouy@nmfs.local>
2024-03-27 13:02:30 -0400

    doc updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
+smart_quotes = False
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
